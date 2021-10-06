a32b6bce40564ca772dc9061f9df1944f3f78ea2
SeungchulHa <halaxhenry@gmail.com>
2021-10-06 11:58:35 +0900

    sending verification email and edit validation

--- README.md
+++ README.md
@@ -39,3 +39,24 @@ function validateEmail(email) {
 const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)\*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 return re.test(String(email).toLowerCase());
 }
+
+### send
+
+1. server < config < sendMail.ts
+
+npm i nodemailer google-auth-library
+
+npm i -D @types/nodemailer
+
+npm uninstall @types/bcrypt @types/jsonwebtoken
+
+npm i -D @types/bcrypt @types/jsonwebtoken
+
+2. go to google console and create new project
+   go to APIs & Services < Credentials < Create Credentials < OAuth
+
+https://developers.google.com/oauthplayground/
+
+1.  click gear button and check on Use your own OAuth credentials then put your project information
+2.  type http://mail.google.com on left side and click authrize APIS
+3.  click Exchange authorization code for tokens and get Refresh Token and Access Token
--- server/controllers/authCtrl.ts
+++ server/controllers/authCtrl.ts
@@ -3,6 +3,10 @@ import Users from '../models/userModel'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { generateAccessToken } from '../config/generateToken'
+import sendEmail from '../config/sendMail'
+import { validPhone, validateEmail} from '../middleware/valid'
+
+const CLIENT_URL = `${process.env.BASE_URL}`
 
 const authCtrl = {
     register: async(req: Request, res: Response) => {
@@ -19,6 +23,13 @@ const authCtrl = {
             const newUser = { name, account, password: passwordHash }
             
             const active_token = generateAccessToken({newUser})
+
+            const url = `${CLIENT_URL}/active/${active_token}`
+
+            if(validateEmail(account)){
+                sendEmail(account, url, 'Verify your email address')
+                return res.json({msg: "Success! Please check your email"})
+            }
 
             res.json({
                 status: 'OK',
