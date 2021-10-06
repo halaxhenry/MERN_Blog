e705a8f6be1a00b2dfc9b53be005c81e1c05d40e
SeungchulHa <halaxhenry@gmail.com>
2021-10-06 22:40:45 +0900

    create login logout refreshToken function

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import { IUser } from '../config/interface'
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -27,10 +28,10 @@ const userSchema = new mongoose.Schema({
     },
     type: {
         type: String,
-        default: 'normal' // social // phone number  // fast
+        default: 'register' // social // phone number  // fast
     }
 }, {
     timestamps: true
 })
 
-export default mongoose.model('User', userSchema)
+export default mongoose.model<IUser>('User', userSchema)
