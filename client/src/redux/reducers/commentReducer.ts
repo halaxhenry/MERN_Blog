05671b3904eb4f55a0a844f792640a1b621bd463
SeungchulHa <halaxhenry@gmail.com>
2021-10-20 22:43:44 +0900

    register and get comment on detail page

--- client/src/redux/reducers/commentReducer.ts
+++ client/src/redux/reducers/commentReducer.ts
@@ -1,30 +1,34 @@
 import {
-    ICommentState,
-    CREATE_COMMENT,
-    ICommentType
-  } from '../types/commentType'
-  
-  const initialState = {
-    data: [],
-    total: 1
-  }
-  
-  
-  const commentReducer = (
-    state: ICommentState = initialState,
-    action: ICommentType
-  ): ICommentState => {
-    switch(action.type){
-      case CREATE_COMMENT:
-        return{
-          ...state,
-          data: [action.payload, ...state.data]
-        }
+  ICommentState,
+  CREATE_COMMENT,
+  GET_COMMENTS,
+  ICommentType
+} from '../types/commentType'
+
+const initialState = {
+  data: [],
+  total: 1
+}
+
+
+const commentReducer = (
+  state: ICommentState = initialState,
+  action: ICommentType
+): ICommentState => {
+  switch(action.type){
+    case CREATE_COMMENT:
+      return{
+        ...state,
+        data: [action.payload, ...state.data]
+      }
+    
+    case GET_COMMENTS:
+      return action.payload
       
-      default:
-        return state
-    }
+    default:
+      return state
   }
-  
-  
-  export default commentReducer
+}
+
+
+export default commentReducer
