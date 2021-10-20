7731b9ab05853dc55c38a62f70673bf390d58ea9
SeungchulHa <halaxhenry@gmail.com>
2021-10-20 18:59:14 +0900

    Blog and User matching

--- client/src/redux/actions/blogAction.ts
+++ client/src/redux/actions/blogAction.ts
@@ -9,7 +9,9 @@ import {
   GET_HOME_BLOGS,
   IGetHomeBlogsType,
   GET_BLOGS_CATEGORY_ID,
-  IGetBlogsCategoryType
+  IGetBlogsCategoryType,
+  GET_BLOGS_USER_ID,
+  IGetBlogsUserType
 } from '../types/blogType'
 
 export const createBlog = (blog: IBlog, token: string) => 
@@ -27,7 +29,7 @@ async (dispatch: Dispatch<IAlertType>) => {
     
     const newBlog = {...blog, thumbnail: url}
     
-    const res: any = await postAPI('blog', newBlog, token)
+    const res = await postAPI('blog', newBlog, token)
     console.log(res)
 
     dispatch({ type: ALERT, payload: { loading: false } })
@@ -42,7 +44,7 @@ async (dispatch: Dispatch<IAlertType | IGetHomeBlogsType>) => {
   try {
     dispatch({ type: ALERT, payload: { loading: true } })
 
-    const res: any = await getAPI('home/blogs')
+    const res = await getAPI('home/blogs')
 
     dispatch({
       type: GET_HOME_BLOGS,
@@ -64,10 +66,32 @@ async (dispatch: Dispatch<IAlertType | IGetBlogsCategoryType>) => {
 
     dispatch({ type: ALERT, payload: { loading: true } })
 
-    const res: any = await getAPI(`blogs/${id}${value}&limit=${limit}`)
+    const res: any = await getAPI(`blogs/category/${id}${value}&limit=${limit}`)
 
     dispatch({
       type: GET_BLOGS_CATEGORY_ID,
+      payload: {...res.data, id, search }
+    })
+    
+    dispatch({ type: ALERT, payload: { loading: false } })
+  } catch (err: any) {
+    dispatch({ type: ALERT, payload: {errors: err.response.data.msg} })
+  }
+}
+
+
+export const getBlogsByUserId = (id: string, search: string) => 
+async (dispatch: Dispatch<IAlertType | IGetBlogsUserType>) => {
+  try {
+    let limit = 3;
+    let value = search ? search : `?page=${1}`;
+
+    dispatch({ type: ALERT, payload: { loading: true } })
+
+    const res: any = await getAPI(`blogs/user/${id}${value}&limit=${limit}`)
+
+    dispatch({
+      type: GET_BLOGS_USER_ID,
       payload: {...res.data, id, search }
     })
     
