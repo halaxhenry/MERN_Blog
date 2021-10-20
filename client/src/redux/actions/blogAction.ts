9fe318f187550996c3cc278ebc617d02efe75576
SeungchulHa <halaxhenry@gmail.com>
2021-10-20 18:21:33 +0900

    pagination

--- client/src/redux/actions/blogAction.ts
+++ client/src/redux/actions/blogAction.ts
@@ -7,7 +7,9 @@ import { ALERT, IAlertType } from '../types/alertType'
 
 import {
   GET_HOME_BLOGS,
-  IGetHomeBlogsType
+  IGetHomeBlogsType,
+  GET_BLOGS_CATEGORY_ID,
+  IGetBlogsCategoryType
 } from '../types/blogType'
 
 export const createBlog = (blog: IBlog, token: string) => 
@@ -25,7 +27,7 @@ async (dispatch: Dispatch<IAlertType>) => {
     
     const newBlog = {...blog, thumbnail: url}
     
-    const res = await postAPI('blog', newBlog, token)
+    const res: any = await postAPI('blog', newBlog, token)
     console.log(res)
 
     dispatch({ type: ALERT, payload: { loading: false } })
@@ -40,11 +42,33 @@ async (dispatch: Dispatch<IAlertType | IGetHomeBlogsType>) => {
   try {
     dispatch({ type: ALERT, payload: { loading: true } })
 
-    const res = await getAPI('home/blogs')
+    const res: any = await getAPI('home/blogs')
 
     dispatch({
       type: GET_HOME_BLOGS,
       payload: res.data
+    })
+    
+    dispatch({ type: ALERT, payload: { loading: false } })
+  } catch (err: any) {
+    dispatch({ type: ALERT, payload: {errors: err.response.data.msg} })
+  }
+}
+
+
+export const getBlogsByCategoryId = (id: string, search: string) => 
+async (dispatch: Dispatch<IAlertType | IGetBlogsCategoryType>) => {
+  try {
+    let limit = 8;
+    let value = search ? search : `?page=${1}`;
+
+    dispatch({ type: ALERT, payload: { loading: true } })
+
+    const res: any = await getAPI(`blogs/${id}${value}&limit=${limit}`)
+
+    dispatch({
+      type: GET_BLOGS_CATEGORY_ID,
+      payload: {...res.data, id, search }
     })
     
     dispatch({ type: ALERT, payload: { loading: false } })
