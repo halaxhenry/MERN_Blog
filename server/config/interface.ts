e67618744c0e1c8457896ae7235070ac7ffc1d84
SeungchulHa <halaxhenry@gmail.com>
2021-10-20 20:22:05 +0900

    backend Comment

--- server/config/interface.ts
+++ server/config/interface.ts
@@ -1,43 +1,56 @@
-import {Document} from 'mongoose'
+import { Document } from 'mongoose'
 import { Request } from 'express'
 
-export interface IUser extends Document {
-    name: string
-    account: string
-    password: string
-    avatar: string
-    role: string
-    type: string,
-    _doc: object
-}
-export interface INewUser {
-    name: string
-    account: string
-    password: string
+export interface IUser extends Document{
+  name: string
+  account: string
+  password: string
+  avatar: string
+  role: string
+  type: string
+  _doc: object
 }
 
-export interface IDecodedToken{
-    id?: string
-    newUser?: INewUser
-    iat: number
-    exp: number
+
+export interface INewUser {
+  name: string
+  account: string
+  password: string
+}
+
+export interface IDecodedToken {
+  id?: string
+  newUser?: INewUser
+  iat: number
+  exp: number
 }
 
 export interface IGgPayload {
-    email: string
-    email_verified: boolean
-    name: string
-    picture: string
+  email: string
+  email_verified: boolean
+  name: string
+  picture: string
 }
 
-export interface IUserParams{
-    name: string
-    account: string
-    password: string
-    avatar?: string
-    type: string
+export interface IUserParams {
+  name: string 
+  account: string 
+  password: string
+  avatar?: string
+  type: string
 }
 
 export interface IReqAuth extends Request {
-    user?: IUser
+  user?: IUser
+}
+
+
+export interface IComment extends Document{
+  user: string
+  blog_id: string
+  blog_user_id: string
+  content: string
+  replyCM: string[]
+  reply_user: string
+  _doc: object
 }
